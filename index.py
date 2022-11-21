6e607529a2ef1f2865e1c60595f3a902b6e4c133
Sara Acuna <acunabenabidessaraelis@gmail.com>
2022-11-20 19:05:51 -0500

    app v1

--- index.py
+++ index.py
@@ -1,14 +1,69 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect,url_for,flash
+from flask_mysqldb import MySQL
 
 app=Flask(__name__)
+# Conecion a mysql
+app.config["MYSQL_HOST"]="localhost"
+app.config["MYSQL_USER"]="root"
+app.config["MYSQL_PASSWORD"]="SaraAcuña123"
+app.config["MYSQL_DB"]="testing"
+mysql=MySQL(app)
+
+# Iniciar una sesion
+app.secret_key = 'mysecretkey'
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    cur=mysql.connection.cursor()
+    cur.execute("SELECT*FROM data")
+    data=cur.fetchall()
+    return render_template("home.html", usuarios=data)
 
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/eliminar/<string:id>')
+def eliminar(id):
+    cur=mysql.connection.cursor()
+    cur.execute("DELETE FROM data where id={0}".format(id))
+    mysql.connection.commit()
+    flash("Usuario eliminado")
+    return redirect(url_for("home"))
+
+@app.route('/editar/<string:id>',methods=['GET','POST'])
+def editar(id):
+    if request.method=="GET":
+        cur=mysql.connection.cursor()
+        cur.execute("SELECT*FROM data where id={0}".format(id))
+        data=cur.fetchall()
+        return render_template("editar.html", usuario=data[0])
+    if request.method=="POST":
+        name=request.form["name"]
+        lastname=request.form["lastname"]
+        cur=mysql.connection.cursor()
+        cur.execute("""
+        UPDATE data 
+        SET name=%s,
+        lastname=%s 
+        WHERE id=%s
+        """,(name, lastname,id))
+        mysql.connection.commit()
+        flash("Contacto actualizado") 
+        return redirect(url_for("home"))  
+
+@app.route('/test', methods=['GET','POST'])
+def test():
+    if request.method=="POST":
+        name=request.form["name"]
+        lastname=request.form["lastname"]
+        cur=mysql.connection.cursor()
+        cur.execute("INSERT INTO data (name, lastname) VALUES (%s,%s )",(name, lastname))
+        mysql.connection.commit()
+        flash("Dato guardado con éxito")
+        return redirect(url_for("home"))
+    if request.method=="GET":
+        return render_template("test.html")
 
 if __name__=='__main__':
     app.run(debug=True)
